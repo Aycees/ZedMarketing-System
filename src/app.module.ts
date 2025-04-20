f64dbd716eb3713d1732b3471113bcb1f6b10067
Adolfo Cedric S. Sarillo <adolfocedric@gmail.com>
2025-04-20 20:07:17 +0800

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AccountsModule } from './accounts/accounts.module';
+import { SupabaseModule } from './supabase/supabase.module';
 
 @Module({
-  imports: [],
+  imports: [AccountsModule, SupabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
