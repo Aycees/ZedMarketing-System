bb7fa39b3ba13ec04816e2d1e8b804217457e21a
Adolfo Cedric S. Sarillo <adolfocedric@gmail.com>
2025-04-22 23:45:57 +0800

    feat: created authentication and implemented it globally

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,14 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { loginAccountDto } from './dto/login-account.dto';
+import { AuthGuard } from './auth.guard';
+import { Public } from './decorator/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Post('login')
   signIn(@Body() loginAccountDto: loginAccountDto) {
@@ -17,4 +20,9 @@ export class AuthController {
     return "logout"
   }
 
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.account;
+  }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,18 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth.guard';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   imports: [
     PrismaModule,
     JwtModule.register({
@@ -15,6 +23,6 @@ import { PrismaModule } from 'src/prisma/prisma.module';
       signOptions: { expiresIn: '60m' },
     }),
   ],
-  exports: [AuthService]
+  exports: [AuthService],
 })
 export class AuthModule {}
